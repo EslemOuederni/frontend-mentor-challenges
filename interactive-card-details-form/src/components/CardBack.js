9e0cfa8eee2a85249cd57c5e38e46baaeedbe884
essss <sousououederni@gmail.com>
2022-09-22 18:44:21 +0100

    back card component

---
+++ interactive-card-details-form/src/components/CardBack.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const CardBack = () => {
+  return (
+    <div className="bg-image-back w-96 h-52 rounded-md text-white">
+      <div class="number">
+        <span class="tracking-widest font-medium text-sm float-right">000</span>
+      </div>
+    </div>
+  );
+};
