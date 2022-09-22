1d8ad55f70adfee1dc1e6c278e0d018f098786ef
essss <sousououederni@gmail.com>
2022-09-22 20:16:23 +0100

    cardback update

--- interactive-card-details-form/src/components/CardBack.js
+++ interactive-card-details-form/src/components/CardBack.js
@@ -2,9 +2,11 @@ import React from "react";
 
 export const CardBack = () => {
   return (
-    <div className="bg-image-back w-96 h-52 rounded-md text-white">
+    <div className="bg-image-back w-96 h-52 rounded-md text-white absolute">
       <div class="number">
-        <span class="tracking-widest font-medium text-sm float-right">000</span>
+        <span class="tracking-widest font-medium text-sm float-right relative top-24 right-5">
+          000
+        </span>
       </div>
     </div>
   );
