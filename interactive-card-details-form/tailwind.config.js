c01a11f1c41a5c8a31562dea6681794a1bc0c734
essss <sousououederni@gmail.com>
2022-09-22 18:45:05 +0100

    theme

--- interactive-card-details-form/tailwind.config.js
+++ interactive-card-details-form/tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: (theme) => theme("colors"),
+      textColor: {
+        primary: {
+          100: "#FF5252",
+        },
+        secondary: {
+          100: "#ffffff",
+          200: "#DEDDDF",
+          300: "#8E8593",
+          400: "#21092F",
+        },
+      },
+      colors: {
+        secondary: {
+          100: "#ffffff",
+          200: "#DEDDDF",
+          300: "#8E8593",
+          400: "#21092F",
+        },
+      },
+    },
   },
   plugins: [],
 };
