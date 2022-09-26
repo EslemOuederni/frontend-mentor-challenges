c4201d8e21403f3e0bd2d384759d34f21dd9350d
essss <sousououederni@gmail.com>
2022-09-26 16:54:15 +0100

    routes

--- interactive-card-details-form/src/App.js
+++ interactive-card-details-form/src/App.js
@@ -1,7 +1,18 @@
 import { Home } from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { CardDetails } from "./pages/CardDetails";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cardDetails" element={<CardDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
