a3716a5684293a797348a928d6687fe2b3b77747
essss <sousououederni@gmail.com>
2022-09-27 14:23:48 +0100

    theme

---
+++ rest-countries-api/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class", '[data-mode="dark"]'],
+  content: [],
+  theme: {
+    extend: {
+      textColor: (theme) => theme("colors"),
+      textColor: {
+        darkMode: {
+          100: "#FFFFFF",
+        },
+        lightMode: {
+          100: "#111517",
+          200: "#858585",
+        },
+      },
+      colors: {
+        darkMode: {
+          100: "#2B3945",
+          200: "#202C37",
+        },
+        lightMode: {
+          100: "#FAFAFA",
+          200: "#FFFFFF",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
