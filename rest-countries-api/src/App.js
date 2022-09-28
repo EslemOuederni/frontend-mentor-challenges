c62cd2685ffb7188abf0ec2c0232ceb2f9becb50
essss <sousououederni@gmail.com>
2022-09-28 22:06:42 +0100

    routes

--- rest-countries-api/src/App.js
+++ rest-countries-api/src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import { HomePage } from "./pages/HomePage";
+import { CountryPage } from "./pages/CountryPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="">
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route index element={<HomePage />}></Route>
+        <Route path="/countries" element={<CountryPage />}>
+          <Route path="/countries/:country" element={<CountryPage />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
