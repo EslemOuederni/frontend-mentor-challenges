82a452fa67674fa7b28576d64ff4ab6bfca32123
essss <sousououederni@gmail.com>
2022-09-28 19:14:16 +0100

    country card

---
+++ rest-countries-api/src/components/CountryCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const CountryCard = ({
+  flag,
+  countryName,
+  population,
+  region,
+  capital,
+  handleClick,
+}) => {
+  return (
+    <div
+      className=" w-60 h-80 bg-lightMode-100 shadow-md ml-12 mb-12 rounded-md cursor-pointer"
+      onClick={handleClick}
+    >
+      <div className="img mb-2 overflow-hidden">
+        <img src={flag} alt="" className="flag border-inherit" />
+      </div>
+      <div className="countryName font-bold ml-2">{countryName}</div>
+      <div className=" ml-2">
+        <p>Population: {population} </p>
+        <p>Region:{region} </p>
+        <p>Capital: {capital}</p>
+      </div>
+    </div>
+  );
+};
