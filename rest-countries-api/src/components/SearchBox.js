9f7ee33a872a1eefb64ff55790ff90af0d988778
essss <sousououederni@gmail.com>
2022-09-29 22:22:49 +0100

    shadow box

--- rest-countries-api/src/components/SearchBox.js
+++ rest-countries-api/src/components/SearchBox.js
@@ -3,11 +3,11 @@ import { BsSearch } from "react-icons/bs";
 export const SearchBox = ({ dark }) => {
   return (
     <div
-      className={`relative block bg-lightMode-200 ml-12 w-96 rounded-md ${dark}`}
+      className={`relative block bg-lightMode-200 ml-12 w-96 rounded-md shadow-md ${dark}`}
     >
       <BsSearch className="pointer-events-none absolute top-1/2 transform -translate-y-1/2 left-9" />
       <input
-        className={`form-input px-4 appearance-none block pl-14 focus:outline-none w-full py-4 px-8s rounded-md ml-6 bg-lightMode-200 shadow-md ${dark}`}
+        className={`form-input px-4 appearance-none block pl-14 focus:outline-none w-full py-4 px-8s rounded-md ml-6 bg-lightMode-200  ${dark}`}
         type="text"
         name="country"
         id="country"
